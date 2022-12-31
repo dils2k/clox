225252b6d9e7a917e4b2b1f2133460f4a1052130
Dils Matchanov <dils.matchanov@gmail.com>
2022-12-31 18:55:11 +0500

    Init commit

---
+++ memory.h
@@ -0,0 +1,11 @@
+#ifndef clox_memory_h
+#define clox_memory_h
+
+#include "common.h"
+
+#define GROW_CAPACITY(capacity) ((capacity) < 8 ? 8 : (capacity) * 2)
+
+#define GROW_ARRAY(type, pointer, oldCount, newCount) \
+  (type*) reallocate(pointer, sizeof(type) * (oldCount), sizeof(type) * (newCount))
+
+#endif
